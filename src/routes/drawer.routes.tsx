f57044b2411ce3aee35f14a3a09c158a85cd23b1
Lázaro <lazarodu@gmail.com>
2021-07-15 15:37:40 -0300

    TintColor Drawer

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Screen, Notification } from "../pages";
+import { color } from "react-native-reanimated";
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +10,8 @@ export default function App() {
     <Drawer.Navigator
       screenOptions={{
         headerShown: true,
+        headerStyle: { backgroundColor: "#009900" },
+        headerTintColor: "white",
       }}
     >
       <Drawer.Screen name="Screen" component={Screen} />
